24cb8ea0f6ff000a85e49d5a51b50268f07c3b68
@Mike-Prybytkin <prybytkin@bk.ru>
2023-01-17 20:49:48 +0300

    feat: implement popup

--- script/index.js
+++ script/index.js
@@ -1,7 +1,9 @@
 import toggleBurgerMenu from './toggle-burger-menu.js';
 import toggleLanguage from './toggle-language.js';
 import toogleTheme from './toggle-theme.js';
+import popup from './popup.js';
 
 toggleBurgerMenu();
 toggleLanguage();
 toogleTheme();
+popup();
